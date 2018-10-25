5444d41907b930147b770c0eb738677fd9d3a56f
Yeniel Landestoy Martín <yeniel.landestoy@mirada.tv>
2018-10-25 11:25:06 +0200

    commits-not-merge has been modified using hg merge to find commits not merge

--- commits-not-merge.sh
+++ commits-not-merge.sh
@@ -2,25 +2,25 @@
 
 hg pull --rev $1
 
-lastMergeChangeset=$(hg log --rev "reverse(merge() and branch($1) and children(branch($2)))" --limit 1 --template "{node}")
-lastCommit=$(hg log -b $2 --limit 1 --template "{node}")
+currentBranch=$(hg branch)
+headOfBranchToMerge=$(hg log -b $1 --limit 1 --template "{node}")
+changesetNotMerged=$(hg merge -P aae8a9f7a238 | grep "changeset" | cut -f3 -d: | tr '\n' '+' | sed 's/.$//g')
 
-commitNotMerged=$(hg log --rev "$lastCommit % $lastMergeChangeset" --template "Changeset:\t{node}|Author:\t\t{author}|Date:\t\t{date|date}|Description:\t\t{desc}||")
-
-blameRanking=$(hg log --rev "$lastCommit % $lastMergeChangeset" --template "{author}\n" | sort | uniq -c | sort -nr | tr -d '\n' | sed -e 's/   /|/g')
+commitsNotMerged=$(hg log -r $changesetNotMerged --template "Changeset:\t{node}|Author:\t\t{author}|Date:\t\t{date|date}|Description:\t\t{desc}||")
+blameRanking=$(hg log -r $changesetNotMerged --template "{author}\n" | sort | uniq -c | sort -nr | tr -d '\n' | sed -e 's/   /|/g')
 
 cp /dev/null email.txt
 
 echo "RECIPIENTS=apple-dev@mirada.tv" >> email.txt
-echo "SUBJECT=Commits de $2 no mergeados en $1" >> email.txt
+echo "SUBJECT=Commits de $1 no mergeados en $currentBranch" >> email.txt
 
-if [ -z "$commitNotMerged" -o ! -n "$commitNotMerged" ]; then
+if [ -z "$commitsNotMerged" -o ! -n "$commitsNotMerged" ]; then
   emailText="TEXT=|TODO MERGEADO, QUE PUTOS CRACKS!"
 else
   emailText="TEXT=|BLAME RANKING"
   emailText="$emailText|$blameRanking"
   emailText="$emailText||COMMITS|"
-  emailText="$emailText|$commitNotMerged"
+  emailText="$emailText|$commitsNotMerged"
 fi
 
 echo "$emailText" >> email.txt
